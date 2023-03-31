99fdc30435ad5572b70eb6cac96b0a97fcaa4419
Ackon Samuel <ackons433@gmail.com>
2023-03-31 11:26:02 +0000

    Configure reducers in store

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,12 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import userLoginSlice from './slices/loginSlice';
+import userLogoutSlice from './slices/logoutSlice';
+import userRegisterSlice from './slices/registerSlice';
 
 const MyMiddlewares = [logger, thunk];
 
 const store = configureStore({
     reducer: {
-
+        userLogin: userLoginSlice.reducer,
+        userLogout: userLogoutSlice.reducer,
+        userRegister: userRegisterSlice.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(MyMiddlewares),
 }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
