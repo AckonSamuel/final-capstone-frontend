2b8c1108bc29a2f6db5100d018f87905c165524e
Ackon Samuel <ackons433@gmail.com>
2023-04-04 08:23:08 +0000

    Include side nav in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-// react-router components
-import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
-// @mui material components
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { CacheProvider } from '@emotion/react';
 import CssBaseline from "@mui/material/CssBaseline";
@@ -11,6 +10,8 @@ import linearGradient from  './theme/linearGradient';
 import rgba from './theme/rgba';
 import hexToRgb from "./theme/hexToRgb";
 import routes from './routes';
+import { useMaterialUIController, setMiniSidenav } from "./context";
+import Sidenav from "./components/SideNav";
 
 const theme = createTheme({
   functions: {
@@ -22,8 +23,33 @@ const theme = createTheme({
 });
 
 const App = () => {
+  const [controller, dispatch] = useMaterialUIController();
+
+  const { miniSidenav, layout } = controller;
+
+  const [onMouseEnter, setOnMouseEnter] = useState(false);
+  const { pathname } = useLocation();
 
   const cache = createEmotionCache();
+
+  const handleOnMouseEnter = () => {
+    if (miniSidenav && !onMouseEnter) {
+      setMiniSidenav(dispatch, false);
+      setOnMouseEnter(true);
+    }
+  };
+
+  const handleOnMouseLeave = () => {
+    if (onMouseEnter) {
+      setMiniSidenav(dispatch, true);
+      setOnMouseEnter(false);
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
 
   const getRoutes = (allRoutes) => allRoutes.map((route) => {
     if (route.collapse) {
@@ -42,6 +68,14 @@ const App = () => {
     <CacheProvider value={cache}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        {layout === "dashboard" && (
+        <Sidenav
+          brandName="ClubWeb"
+          routes={routes}
+          onMouseEnter={handleOnMouseEnter}
+          onMouseLeave={handleOnMouseLeave}
+        />
+      )}
           <Routes>{getRoutes(routes)}</Routes>
       </ThemeProvider>
     </CacheProvider>
