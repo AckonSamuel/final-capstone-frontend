b8089dc8eb97f6062677658bc4701833a63f4ffa
Ackon Samuel <ackons433@gmail.com>
2023-04-04 08:25:33 +0000

    Create dashboard skeleton

---
+++ src/components/Dashboard/index.jsx
@@ -0,0 +1,13 @@
+import DashboardLayout from "../layouts/DashboardLayout";
+import Navbar from "./../Navbar";
+function Overview() {
+
+  return (
+    <DashboardLayout>
+      <Navbar />
+      <>hey</>
+    </DashboardLayout>
+  );
+}
+
+export default Overview;
