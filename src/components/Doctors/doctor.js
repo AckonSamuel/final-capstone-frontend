c47a905dcb421ab771b380f329ae4b4af5101bc4
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-04-08 15:24:24 +0500

    Add dummy data for doctors

---
+++ src/components/Doctors/doctor.js
@@ -0,0 +1,20 @@
+const doctors = [
+  {
+    id: '1',
+    name: 'Dr. John Smith',
+    major: 'Cardiology',
+    availability: 'Monday - Friday, 9am - 5pm',
+    price: '$200 per consultation',
+    picture: 'https://www.shutterstock.com/image-photo/portrait-her-she-nicelooking-attractive-260nw-1729215223.jpg',
+  },
+  {
+    id: '2',
+    name: 'Dr. Jane Doe',
+    major: 'Pediatrics',
+    availability: 'Tuesday - Thursday, 10am - 6pm',
+    price: '$150 per consultation',
+    picture: 'https://thumbs.dreamstime.com/b/male-doctor-portrait-isolated-white-background-56744085.jpg',
+  },
+];
+
+export default doctors;
