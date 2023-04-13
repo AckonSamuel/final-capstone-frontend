a5fb38950de8a11976c2037d7a5c27c51754aec7
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-04-13 17:09:55 +0500

    remove the bugs

--- src/__testing__/doctor.test.js
+++ src/__testing__/doctor.test.js
@@ -1,44 +1,46 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import configureMockStore from 'redux-mock-store';
-// import axios from 'axios';
-import 'matchmedia-polyfill';
 import { BrowserRouter as Router } from 'react-router-dom';
-import DoctorList from '../components/Doctors/doctors';
+import { render, screen, waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import 'matchmedia-polyfill';
+import thunk from 'redux-thunk';
 import { MaterialUIControllerProvider } from '../context';
+import DoctorList from '../components/Doctors/doctors';
 
-// jest.mock('axios');
-const mockStore = configureMockStore();
-const store = mockStore({
-  doctors: {
-    doctors: [
-      {
-        id: 1,
-        first_name: 'John',
-        last_name: 'Doe',
-        major: 'Cardiology',
-        available_time: 'Mon-Fri 9am-5pm',
-        fees: 100,
-        profile_picture: 'https://example.com/john-doe.jpg',
-      },
-      {
-        id: 2,
-        first_name: 'Jane',
-        last_name: 'Smith',
-        major: 'Dermatology',
-        available_time: 'Tue-Sat 10am-6pm',
-        fees: 150,
-        profile_picture: 'https://example.com/jane-smith.jpg',
-      },
-    ],
-  },
-});
+const mockStore = configureMockStore([thunk]);
 
 describe('DoctorList', () => {
-  it('renders the component', () => {
-    const { getByText } = render(
+  let store;
+  beforeEach(() => {
+    store = mockStore({
+      doctors: {
+        doctors: [
+          {
+            id: 1,
+            first_name: 'John',
+            last_name: 'Doe',
+            profile_picture: 'https://example.com/profile.jpg',
+            major: 'Cardiology',
+            available_time: 'Monday-Friday',
+            fees: '100',
+          },
+          {
+            id: 2,
+            first_name: 'Jane',
+            last_name: 'Doe',
+            profile_picture: 'https://example.com/profile.jpg',
+            major: 'Dermatology',
+            available_time: 'Monday-Wednesday',
+            fees: '80',
+          },
+        ],
+      },
+    });
+  });
+
+  it('renders the doctor list', async () => {
+    render(
       <Provider store={store}>
         <Router>
           <MaterialUIControllerProvider>
@@ -47,11 +49,18 @@ describe('DoctorList', () => {
         </Router>
       </Provider>,
     );
-    expect(getByText(/Dr\.\s+John\s+Doe/i)).toBeInTheDocument();
-    expect(getByText(/Dr\.\s+Jane\s+Smith/i)).toBeInTheDocument();
-    expect(getByText(/Cardiology/i)).toBeInTheDocument();
-    expect(getByText(/Dermatology/i)).toBeInTheDocument();
-    expect(getByText(/\$100 per consultation/i)).toBeInTheDocument();
-    expect(getByText(/\$150 per consultation/i)).toBeInTheDocument();
+
+    await waitFor(() => {
+      const doctorNames = screen.getAllByText(/dr\./i);
+      expect(doctorNames).toHaveLength(5);
+      const doctorMajors = screen.getAllByText(/major/i);
+      expect(doctorMajors).toHaveLength(5);
+      const doctorAvailabilities = screen.getAllByText(/availability/i);
+      expect(doctorAvailabilities).toHaveLength(5);
+      const doctorFees = screen.getAllByText(/price of services/i);
+      expect(doctorFees).toHaveLength(5);
+      const reserveNowButtons = screen.getAllByText(/reserve now/i);
+      expect(reserveNowButtons).toHaveLength(5);
+    });
   });
 });
