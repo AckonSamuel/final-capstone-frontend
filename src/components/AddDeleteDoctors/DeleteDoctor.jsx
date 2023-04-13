0b8c17ab1dd97d690ac9e15f16836d822c452711
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-04-13 15:56:47 +0300

    Adds the html and css for doctor delete page

--- src/components/AddDeleteDoctors/DeleteDoctor.jsx
+++ src/components/AddDeleteDoctors/DeleteDoctor.jsx
@@ -3,12 +3,12 @@ import './deletedoctor.css';
 import axios from 'axios';
 
 const DeleteDoctor = () => {
-  const [doctors, setDoctors] = useState({});
+  const [doctors, setDoctors] = useState([]);
 
   useEffect(() => {
     const setDoctorsFunction = async () => {
       const response = await axios.get('http://localhost:3000/api/v1/doctors');
-      setDoctors(response);
+      setDoctors(response.data);
     };
     setDoctorsFunction();
   }, []);
@@ -17,6 +17,32 @@ const DeleteDoctor = () => {
   return (
     <div className="delete-doctor-page">
       <h3>Delete A Doctor</h3>
+      <ul className="each-doctor-ul">
+        {doctors
+        && doctors.map((doctor) => (
+          <li key={doctor.id}>
+            <img src={doctor.profile_picture} alt={doctor.profile_picture} />
+            <span>
+              Dr
+              {' '}
+              {doctor.first_name}
+              {' '}
+              {doctor.last_name}
+            </span>
+            <span>
+              Major:
+              {' '}
+              {doctor.major}
+            </span>
+            <span>
+              Fees:
+              {' '}
+              {doctor.fees}
+              $
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
