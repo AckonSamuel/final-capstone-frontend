9c99cfedce02affac672bf3afbd2be17320b5a81
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-04-13 14:51:37 +0300

    Adds the post request for adding a doctor

--- src/components/AddDeleteDoctors/AddDoctor.jsx
+++ src/components/AddDeleteDoctors/AddDoctor.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable quote-props */
 /* eslint jsx-a11y/label-has-associated-control: ["error", { assert: "either" } ] */
 
 import React, { useState } from 'react';
@@ -10,11 +11,41 @@ const AddDoctor = () => {
   const [profilePicture, setProfilePicture] = useState('');
   const [fees, setFees] = useState('');
   const [availableTime, setAvailableTime] = useState('');
+  const [addedMessage, setAddedMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:3000/doctors', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(
+        {
+          'first_name': `${firstName}`,
+          'last_name': `${lastName}`,
+          'major': `${major}`,
+          'profile_picture': `${profilePicture}`,
+          fees,
+          'available_time': `${availableTime}`,
+        },
+      ),
+    }).then(() => {
+      setFirstName('');
+      setLastName('');
+      setMajor('');
+      setProfilePicture('');
+      setFees('');
+      setAvailableTime('');
+      setAddedMessage('A new doctor has been added successfully');
+      setTimeout(() => {
+        setAddedMessage('');
+      }, '2000');
+    });
+  };
 
   return (
     <div className="create">
       <h4>Add A New Doctor</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="first-name">First Name</label>
         <input
           type="text"
@@ -69,7 +100,8 @@ const AddDoctor = () => {
           onChange={(e) => setAvailableTime(e.target.value)}
         />
 
-        <button type="button">Add Doctor</button>
+        <button type="submit">Add Doctor</button>
+        <p className="added-message">{addedMessage}</p>
       </form>
     </div>
   );
