f981595f691d1fa0d112f1b1af8b31ad1deb11eb
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-04-13 15:08:22 +0300

    Adds the get request to fetch doctors

--- src/components/AddDeleteDoctors/DeleteDoctor.jsx
+++ src/components/AddDeleteDoctors/DeleteDoctor.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './deletedoctor.css';
+import axios from 'axios';
 
 const DeleteDoctor = () => {
-  const doctor = {
-    infinite: true,
-    dots: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-  };
+  const [doctors, setDoctors] = useState({});
+
+  useEffect(() => {
+    const setDoctorsFunction = async () => {
+      const response = await axios.get('http://localhost:3000/api/v1/doctors');
+      setDoctors(response);
+    };
+    setDoctorsFunction();
+  }, []);
+
+  console.log(doctors);
   return (
     <div className="delete-doctor-page">
       <h3>Delete A Doctor</h3>
